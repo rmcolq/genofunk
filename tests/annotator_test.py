e2b2063166f775a5f944c6ade98038f94c67c1c9
Rachel Colquhoun <rmnorris@well.ox.ac.uk>
2019-10-30 15:22:24 +0000

    first tests run

--- tests/annotator_test.py
+++ tests/annotator_test.py
@@ -2,6 +2,7 @@ import filecmp
 import os
 import unittest
 import glob
+import json
 
 from genofunk import annotator
 
@@ -9,40 +10,41 @@ this_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data_dir = os.path.join(this_dir, 'tests', 'data', 'annotator')
 
 class TestAnnotator(unittest.TestCase):
-    def load_reference_info_no_file(self):
+    def test_load_reference_info_no_file(self):
         ref_filepath = os.path.join(data_dir, 'idontexist.json')
-
-        self.assertRaises(TypeError, annotator.Annotator.load_reference_info(ref_filepath))
-
-    def load_reference_info_empty_file(self):
-        ref_filepath = os.path.join(data_dir, 'empty_ref.json')
-
-        self.assertRaises(TypeError, annotator.Annotator.load_reference_info(ref_filepath))
-
-    def load_reference_info_no_references(self):
-        ref_filepath = os.path.join(data_dir, 'no_references_ref.json')
-
-        self.assertRaises(TypeError, annotator.Annotator.load_reference_info(ref_filepath))
-
-    def load_reference_info_no_sequence(self):
-        ref_filepath = os.path.join(data_dir, 'missing_sequence_ref.json')
-
-        self.assertRaises(TypeError, annotator.Annotator.load_reference_info(ref_filepath))
-
-    def load_reference_info_no_orf(self):
-        ref_filepath = os.path.join(data_dir, 'missing_orf_ref.json')
-
-        self.assertRaises(TypeError, annotator.Annotator.load_reference_info(ref_filepath))
-
-    def load_reference_info_missing_accession(self):
-        ref_filepath = os.path.join(data_dir, 'missing_accession_ref.json')
-
         a = annotator.Annotator("accession")
-        self.assertRaises(TypeError, a.load_reference_info(ref_filepath))
+        self.assertRaises(FileNotFoundError, a.load_reference_info, ref_filepath)
 
-    def load_reference_info_correct_accession(self):
+    def test_load_reference_info_empty_file(self):
+        ref_filepath = os.path.join(data_dir, 'empty_ref.json')
+        a = annotator.Annotator("accession")
+        self.assertRaises(json.decoder.JSONDecodeError, a.load_reference_info, ref_filepath)
+
+    def test_load_reference_info_no_references(self):
+        ref_filepath = os.path.join(data_dir, 'no_references_ref.json')
+        a = annotator.Annotator("accession")
+        self.assertRaises(AssertionError, a.load_reference_info, ref_filepath)
+
+    def test_load_reference_info_no_sequence(self):
+        ref_filepath = os.path.join(data_dir, 'missing_sequence_ref.json')
+        a = annotator.Annotator("accession")
+        self.assertRaises(AssertionError, a.load_reference_info, ref_filepath)
+
+    def test_load_reference_info_no_orf(self):
+        ref_filepath = os.path.join(data_dir, 'missing_orf_ref.json')
+        a = annotator.Annotator("accession")
+        self.assertRaises(AssertionError, a.load_reference_info, ref_filepath)
+
+    def test_load_reference_info_missing_accession(self):
         ref_filepath = os.path.join(data_dir, 'missing_accession_ref.json')
+        a = annotator.Annotator("accession")
+        self.assertRaises(AssertionError, a.load_reference_info, ref_filepath)
 
+    def test_load_reference_info_correct_accession(self):
+        ref_filepath = os.path.join(data_dir, 'missing_accession_ref.json')
         a = annotator.Annotator("LN854563.1")
-        a.load_reference_info(ref_filepath)
-        self.assertIsNotNone(a.reference_info)
+        data = a.load_reference_info(ref_filepath)
+        print(a)
+        print(a.closest_accession)
+        print(data)
+        self.assertIsNotNone(data)
